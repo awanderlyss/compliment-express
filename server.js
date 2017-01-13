995151f0c369acb5bcb4e3a79f4aadf641a422c2
awanderlyss <alyssa.felix7@gmail.com>
2017-01-12 20:50:49 -0500

    Display random comp & give bg random color

--- server.js
+++ server.js
@@ -11,9 +11,32 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+];
+
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.render('compliments-index');
+  var randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  
+  res.render('compliments-index', {
+    compliment: randomCompliment,
+    color: randomColor
+  });
+});
+
+app.get("/:name", (req, res) => {
+  res.send(`hello ${req.params.name}`);
 });
 
 app.listen(3000, function() {
